8b95f1eb4b458ea30d868b1a3d408a0bf9a6390d
Kevin Hinze <Hinze57@users.noreply.github.com>
2017-03-09 11:10:44 -0700

    Add files via upload
    added sourcing an external configFile

--- script-template.sh
+++ script-template.sh
@@ -3,8 +3,6 @@
 ############################################
 # File: script_name.sh
 #
-# Version: 1.0 
-#
 # Purpose: This is a script template ....
 #          
 # Modification History (ddMonYYYY): 
@@ -19,9 +17,15 @@
 ######################
 # Variable Section
 ######################
+## script_version
+version=1.0.0
 ## Change as suits site/location
 # set hostName var to what is configured on the system
 hostName=$(echo $(hostname -s) | tr "[:lower:]" "[:upper:]")
+# point configFile to file containing variables/functions you want to import/read in
+configFile=$(dirname $0)/local.conf
+# source/import the above configFile
+source ${configFile}
 
 ## Shouldn't need to change anything below this line
 # set up tmp dir for working files 
